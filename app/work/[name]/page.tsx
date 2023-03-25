e5336e95eabbeaa07cea50201edfa1862ceee5dc
snenenenenenene <sennebels@gmail.com>
2023-03-25 18:58:55 +0100

    feat: :fire: help

--- app/work/[name]/page.tsx
+++ app/work/[name]/page.tsx
@@ -1,27 +1,46 @@
-import { images } from "@/data/images";
-import Image from "next/image";
+"use client";
+import PocketBase from "pocketbase";
+import { useEffect, useState } from "react";
 
-export default function Project(context: any) {
+export default function ProjectPage(context: any) {
   const name = context.params.name;
 
-  const collection = images.find((image) => image.title === name);
+  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKET_BASE_URL);
+
+  const [collection, setCollection]: any = useState({});
+
+  useEffect(() => {
+    pb.collection("collections")
+      .getFullList(1, { filter: `title="${name}"`, expand: "images" })
+      .then((res: any) => {
+        console.log(res[0]);
+        setCollection(res[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <main className="flex flex-col w-screen h-screen pt-32 relative">
+    <main className="flex flex-col w-full h-full px-20 pt-20 text-black font-sunflower">
       {collection && (
         <>
-          <div className={`gap-x-4 h-5/6 px-12 flex overflow-x-scroll w-full`}>
-            <section className="w-full h-full relative rounded-xl overflow-hidden">
-              <Image
-                src={collection.coverPath}
-                alt="fauna-showcase-pic"
-                className=" w-[100vw] overflow-hidden object-cover"
-                fill
-              />
-            </section>
-            {collection.images && collection.images.map(() => <></>)}
-          </div>
-          <div className="h-1/6"></div>
+          <h1 className="text-8xl">{collection?.title}</h1>
+          <section className="w-full h-full flex gap-x-10">
+            {collection?.expand?.images?.map((image: any, index: number) => (
+              <picture
+                className="w-[20rem] h-[40rem] overflow-hidden"
+                key={index}
+              >
+                <img
+                  loading="lazy"
+                  className="w-full h-full object-cover"
+                  alt={image?.title}
+                  src={`https://zita-website.pockethost.io/api/files/6w4ddsf4nwbdis4/${image?.id}/${image?.src}`}
+                />
+              </picture>
+            ))}
+          </section>
         </>
       )}
     </main>

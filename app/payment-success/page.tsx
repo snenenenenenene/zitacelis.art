45248e2cf30808797a5b0472968c3c86b8efaaef
snenenenenenene <sennebels@gmail.com>
2023-03-29 22:19:58 +0200

    fix: :bug: fixed pb export error

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -9,7 +9,7 @@ import { formatter } from "../config/config";
 import { useStore } from "../utils/store";
 
 import PocketBase from "pocketbase";
-export const pb = new PocketBase(
+const pb = new PocketBase(
   process.env.NEXT_PUBLIC_POCKET_BASE_URL
 ).autoCancellation(false);
 
